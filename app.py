14f410b0940bf1a696598535abf489f7c855a2f3
art <art@serv.com>
2023-11-29 21:03:58 +0300

    some ui prettier (add button to add tasks)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import customtkinter
 
+import core.utils.todo
 from core.settings import Settings
 from core.ui.components.ctknavbar import CtkNavbar
 
@@ -24,12 +25,14 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(0, weight=1)
 
         nav = CtkNavbar(master=self, default_page=0, end_buttons_count=1, label_text="Todo App")
-        self.todo_page = nav.add_page(button_text="[ To Do ]")
+
+        page = core.utils.todo.ToDoFrame(master=nav)
+        self.todo_page = nav.add_page(button_text="[ To Do ]", frame=page)
         self.done_page = nav.add_page(button_text="[Already Done]")
         self.settings_page = nav.add_page(button_text="Settings")
         nav.grid(row=0, column=0, sticky="nsew")
 
-        todo = ToDo()
+        todo = ToDo(frame=self.todo_page)
         todo.new_task(title="Task 1", text="lskjdfalskdhfsadfsakjdflkjsadhflkhsadfkjhsadf")
         todo.new_task(title="Task 2", text="lskjdfalskdhfsadfsakjdflkjsadhflkhsadfkjhsadf")
         todo.new_task(title="Task 3", text="lskjdfalskdhfsadfsakjdflkjsadhflkhsadfkjhsadf")
@@ -37,7 +40,7 @@ class App(customtkinter.CTk):
         todo.new_task(title="Task 5", text="lskjdfalskdhfsadfsakjdflkjsadhflkhsadfkjhsadf")
         todo.new_task(title="Task 6", text="lskjdfalskdhfsadfsakjdflkjsadhflkhsadfkjhsadf")
 
-        todo.pack_tasks_at_frame(frame=self.todo_page, _already_done=False)
+        todo.pack_tasks_at_frame(_already_done=False)
 
     # def change_appearance_mode_event(self, new_appearance_mode: str):
     #     customtkinter.set_appearance_mode(new_appearance_mode)
--- core/utils/todo.py
+++ core/utils/todo.py
@@ -18,9 +18,34 @@ class Task:
     created_at: datetime = datetime.today()
 
 
+class ToDoFrame(ctk.CTkFrame):
+    def __init__(self,
+                 master,
+                 **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        self.todopage = ctk.CTkScrollableFrame(master=self, fg_color="green")
+
+        self.todopage.grid_columnconfigure(0, weight=1)
+        self.todopage.grid(row=0, column=0, columnspan=2, sticky="nsew")
+
+        self.title_input = ctk.CTkEntry(master=self)
+        self.text_input = ctk.CTkEntry(master=self)
+        self.add_button = ctk.CTkButton(master=self)
+
+        self.title_input.grid(row=1, column=0, sticky="nsew")
+        self.text_input.grid(row=1, column=1, sticky="nsew")
+        self.add_button.grid(row=2, column=0, columnspan=2, sticky="nsew")
+
+
 class ToDo:
-    def __init__(self):
+    def __init__(self, frame):
         self._task_list: list[Task] = []
+        self._already_done_task_list: list[Task] = []
+        self._frame = frame
 
     def new_task(self, title: str, text: str):
         new_task = Task(id=self._get_new_task_id(), title=title, text=text)
@@ -29,9 +54,10 @@ class ToDo:
     def add_task(self, task: Task):
         self._task_list.append(task)
 
-    def get_task(self, task_index: int) -> Task | None:
-        if 0 < task_index < len(self._task_list):
-            return self._task_list[task_index]
+    def get_task_index(self, task: Task) -> int | None:
+        for i, task_i in enumerate(self._task_list):
+            if task_i.id == task.id:
+                return i
         return None
 
     def get_all_tasks(self) -> list[Task]:
@@ -53,26 +79,37 @@ class ToDo:
         """
         pass
 
-    def pack_tasks_at_frame(self, frame, _already_done: bool = None):
+    def pack_tasks_at_frame(self, _already_done: bool = None):
         if _already_done is not None:
-            task_list = [i for i in self._task_list if i.already_done == _already_done]
+            task_list: list[Task] = [i for i in self._task_list if i.already_done == _already_done]
         else:
-            task_list = self._task_list
-        for i, task_unit in enumerate([TaskUnit(master=frame, task=task_i) for task_i in task_list]):
+            task_list: list[Task] = self._task_list
+        task_list = sorted(task_list, key=lambda x: x.already_done)
+        for i, task_unit in enumerate(
+                [TaskUnit(master=self._frame.todopage,
+                          task=task_i,
+                          _checkbox_callback=self._checkbox_callback)
+                 for task_i in task_list]):
             task_unit.grid(row=i, column=0, padx=PADDING, pady=(PADDING, 0), sticky="new")
 
     def _get_new_task_id(self) -> int:
         return max([i.id for i in self._task_list]) + 1 if self._task_list else 0
+
+    def _checkbox_callback(self, task: Task):
+        self._task_list[self.get_task_index(task)] = task
+        self.pack_tasks_at_frame()
 
 
 class TaskUnit(ctk.CTkFrame):
     def __init__(self,
                  master,
                  task: Task,
+                 _checkbox_callback,
                  **kwargs):
         super().__init__(master, **kwargs)
 
         self._task: Task = task
+        self._checkbox_callback = _checkbox_callback
 
         self.grid_columnconfigure(1, weight=1)
         self.configure(corner_radius=10)
@@ -106,5 +143,6 @@ class TaskUnit(ctk.CTkFrame):
         self.checkbox.grid(row=0, column=2, rowspan=2, padx=TASK_UNIT_PAD, pady=TASK_UNIT_PAD, sticky="nse")
 
     def _checkbox_event(self):
-        print(self.checkbox_var.get(), self._task.id)
-
+        self._task.already_done = True if self.checkbox_var.get() == "on" else False
+        self._checkbox_callback(task=self._task)
+        # print(self.checkbox_var.get(), self._task.id)
